b64d1a45fdbf17c678af37a154244e7f9136b6e4
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-08-02 12:30:20 -0400

    Add story text to page

--- main.js
+++ main.js
@@ -1,49 +1,88 @@
-const header = $(".header-box");
+//declare JQuery DOM elements
 
-const storyBox = $(".story-box");
+const header = $(".header-box");
 
 $(header).text("Test Header");
 
+const storyBox = $(".story-box");
+
 const post = $(".post");
 
+//declare global values
+// let bestStories = [];
 
-
-
-const mainList = [];
-
-
-
-
-//Create new post for the user to see/interact with. obj is the storyAPI, idNum is the unique
-//story id.
-function makePost (obj, idNum) {
-
+//Create new post for the user to see/interact with.
+//data is s specific story here, fetched from the /item/ folder
+function makePost(data) {
+  if (data.type === "story") {
     //This div houses all the elements of the post
     const newPost = $('<div class="post">');
 
     //Assign this post a uniqe id
-    $(newPost).attr("id", idNum);
-
-    $(newPost).append($('<div class="info-panel">'));
+    $(newPost).attr("id", data.id);
 
     //This div houses the story and comment panel.
     const tempStoryPanel = $('<div class="ST_CO-panel">');
 
-        $(tempStoryPanel).append('<div class="story-panel">');
-        $(tempStoryPanel).append('<div class="comments-panel">');
+    //info panel will house hide, go to comments, score, etc...
+    const storyInfoPanel = $('<div class="info-panel">');
+
+    //Get the score of the story
+    const storyScore = data.score;
+
+    //Display that score to the user
+    $(storyInfoPanel).text(storyScore);
+
+    //Add this panel to the story
+    $(tempStoryPanel).append(storyInfoPanel);
+    $(newPost).append($(storyInfoPanel));
+
+    //will be text of story title, click to go to page
+    const storyPanel = $('<div class="story-panel">');
+
+    const storyName = data.title;
+
+    $(storyPanel).text(storyName);
+
+    $(tempStoryPanel).append(storyPanel);
+
+    //user who posted the story, comments # and click to view
 
     //Add the div, with story & comments, to the post
     $(newPost).append($(tempStoryPanel));
 
-    //I think here, I should add the post to the array,
-    //Then use a different function, to add the posts to the display area.
-    //This could help with sorting.
-    //
     //Add the post to the display area.
     $(storyBox).append(newPost);
+  } else {
+    return;
+  }
+}
 
-} 
+fetch("https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty")
+  // Run the following after receiving a response from the API
+  .then(function (rawData) {
+    // console.log("Raw data received.");
+       console.log(rawData);
 
+    return rawData.json();
+  })
 
+  // Run the following after parsing the data form the response
+  .then(function (data) {
 
+    for (let i = 1; i <= data.length; i++) {
+      populateMainList(i);
+    }
+  });
 
+function populateMainList(storyId) {
+  fetch("https://hacker-news.firebaseio.com/v0/item/" + storyId + ".json?print=pretty")
+
+    .then(function (rawData) {
+      return rawData.json();
+    })
+
+    .then(function (data) {
+      makePost(data);
+    });
+}
